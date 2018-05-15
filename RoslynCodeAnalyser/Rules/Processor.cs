7498dcf0e7df002217a842857ede198764351725
Rohit Tiwari <rohit.beawar@gmail.com>
2018-05-15 16:33:47 +0530

    extract methods

--- RoslynCodeAnalyser/Rules/Processor.cs
+++ RoslynCodeAnalyser/Rules/Processor.cs
@@ -28,55 +28,83 @@ namespace Rules
         {
             var container = new Container();
             container.Configure(p => { p.AddRegistry<BusinessRegistry>(); });
-
             var solutionAnalyser = container.GetInstance<ISolutionAnalyser>();
             SolutionAnalysisData result = solutionAnalyser.Analyse(_solutionpath, _projects);
             List<ProjectMetrics> metricsList = new List<ProjectMetrics>();
+            AnalyzeProject(result, metricsList);
+            return metricsList;
+        }
+
+        private static void AnalyzeProject(SolutionAnalysisData result, List<ProjectMetrics> metricsList)
+        {
             foreach (var prj in result.Projects)
             {
                 ProjectMetrics metrics = new ProjectMetrics();
                 metrics.Projectname = prj.Project.Name;
-                foreach (var cls in prj.Classes)
-                {
-                    ClassMetrics clsMetrics = new ClassMetrics();
-                    clsMetrics.Classname = cls.ClassDeclarationSyntax.Identifier.ValueText;
-                    clsMetrics.Classdocpath = cls.ClassDeclarationSyntax.SyntaxTree.FilePath;
-                    clsMetrics.Namespacename = cls.Namespace.Namespacedeclaration.Name.ToString();
-                    SemanticModel sclsmodel =
-                        prj.CompiledProject.GetSemanticModel(cls.ClassDeclarationSyntax.SyntaxTree);
-                    LocRule locRule = new LocRule();
-                    clsMetrics.Loc = locRule.GetLocCount(cls.ClassDeclarationSyntax);
-                    CyclomaticComplexityCounter cyclomaticComplexityCounter = new CyclomaticComplexityCounter();
-                    clsMetrics.CyclomaticComplexity = cyclomaticComplexityCounter.Calculate(cls.ClassDeclarationSyntax,
-                        sclsmodel);
-
-                    foreach (var mtd in cls.Methods)
-                    {
-
-                        MethodMetrics mtdMetrics = new MethodMetrics();
-                        mtdMetrics.Loc = locRule.GetLocCount(mtd.MethodDeclarationSyntax);
-                        SemanticModel smtdmodel =
-                            prj.CompiledProject.GetSemanticModel(cls.ClassDeclarationSyntax.SyntaxTree);
-                        mtdMetrics.CyclomaticComplexity =
-                            cyclomaticComplexityCounter.Calculate(mtd.MethodDeclarationSyntax, smtdmodel);
-                        MethodparametersRule mtdprmrule = new MethodparametersRule();
-                        mtdMetrics.Methodparameters = mtdprmrule.GetMethodparametersCount(mtd.MethodDeclarationSyntax);
-                        clsMetrics.MethodMetricses.Add(mtdMetrics);
-                    }
-                    foreach (var prop in cls.Properties)
-                    {
-                        break;
-                    }
-                    metrics.ClassMetrics.Add(clsMetrics);
-                }
-                foreach (var inf in prj.Interfaces)
-                {
-                    break;
-                }
+                AnalyzeClass(prj, metrics);
+                AnalyzeInterface(prj, metrics);
                 metricsList.Add(metrics);
             }
+        }
 
-            return metricsList;
+        private static void AnalyzeInterface(ProjectAnalysisData prj, ProjectMetrics metrics)
+        {
+            foreach (var inf in prj.Interfaces)
+            {
+                break;
+            }
+        }
+
+        private static void AnalyzeClass(ProjectAnalysisData prj, ProjectMetrics metrics)
+        {
+            foreach (var cls in prj.Classes)
+            {
+                ClassMetrics clsMetrics = new ClassMetrics();
+                CalculateClassMetrics(prj, clsMetrics, cls);
+                AnalyzeMethod(prj, cls, clsMetrics);
+                AnalyzePropertie(prj, cls, clsMetrics);
+                metrics.ClassMetrics.Add(clsMetrics);
+            }
+        }
+
+        private static void CalculateClassMetrics(ProjectAnalysisData prj, ClassMetrics clsMetrics, ClassAnalysisData cls)
+        {
+            clsMetrics.Classname = cls.ClassDeclarationSyntax.Identifier.ValueText;
+            clsMetrics.Classdocpath = cls.ClassDeclarationSyntax.SyntaxTree.FilePath;
+            clsMetrics.Namespacename = cls.Namespace.Namespacedeclaration.Name.ToString();
+            SemanticModel sclsmodel =
+                prj.CompiledProject.GetSemanticModel(cls.ClassDeclarationSyntax.SyntaxTree);
+            LocRule locRule = new LocRule();
+            clsMetrics.Loc = locRule.GetLocCount(cls.ClassDeclarationSyntax);
+            CyclomaticComplexityCounter cyclomaticComplexityCounter = new CyclomaticComplexityCounter();
+            clsMetrics.CyclomaticComplexity = cyclomaticComplexityCounter.Calculate(cls.ClassDeclarationSyntax,
+                sclsmodel);
+        }
+
+        private static void AnalyzePropertie(ProjectAnalysisData prj, ClassAnalysisData cls, ClassMetrics clsMetrics)
+        {
+            foreach (var prop in cls.Properties)
+            {
+                break;
+            }
+        }
+
+        private static void AnalyzeMethod(ProjectAnalysisData prj, ClassAnalysisData cls, ClassMetrics clsMetrics)
+        {
+            foreach (var mtd in cls.Methods)
+            {
+                CyclomaticComplexityCounter cyclomaticComplexityCounter = new CyclomaticComplexityCounter();
+                LocRule locRule = new LocRule();
+                MethodMetrics mtdMetrics = new MethodMetrics();
+                mtdMetrics.Loc = locRule.GetLocCount(mtd.MethodDeclarationSyntax);
+                SemanticModel smtdmodel =
+                    prj.CompiledProject.GetSemanticModel(cls.ClassDeclarationSyntax.SyntaxTree);
+                mtdMetrics.CyclomaticComplexity =
+                    cyclomaticComplexityCounter.Calculate(mtd.MethodDeclarationSyntax, smtdmodel);
+                MethodparametersRule mtdprmrule = new MethodparametersRule();
+                mtdMetrics.Methodparameters = mtdprmrule.GetMethodparametersCount(mtd.MethodDeclarationSyntax);
+                clsMetrics.MethodMetricses.Add(mtdMetrics);
+            }
         }
     }
 }
